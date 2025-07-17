b647ebdbfcd359e1baf4a2ce24cd1fe66752936b
Bhairav Sharad <bhairav.sharad@gmail.com>
2025-07-17 13:48:07 -0700

    Updated the terminal links to ther terminals in the python file

--- app/main.py
+++ app/main.py
@@ -8,10 +8,10 @@ templates = Jinja2Templates(directory="templates")
 
 # Define file paths for 4 terminals
 TERMINALS = {
-    "Terminal 1": "C:/path/to/Files/Terminal_1.json",
-    "Terminal 2": "C:/path/to/Files/Terminal_2.json",
-    "Terminal 3": "C:/path/to/Files/Terminal_3.json",
-    "Terminal 4": "C:/path/to/Files/Terminal_4.json",
+    "Terminal 1": "C:\Users\3112923-2\AppData\Roaming\MetaQuotes\Terminal\5D49F47D1EA1ECFC0DDC965B6D100AC5\MQL4\Files\Terminal_60044391.json",
+    "Terminal 2": "C:\Users\3112923-2\AppData\Roaming\MetaQuotes\Terminal\6D8ED79B81D9D70041DDDF6786110ECE\MQL4\Files\Terminal_50055879.json",
+    "Terminal 3": "C:\Users\3112923-2\AppData\Roaming\MetaQuotes\Terminal\704444BB97FFEAFF0FAD48FB79A73215\MQL4\Files\Terminal_700000032.json",
+    "Terminal 4": "C:\Users\3112923-2\AppData\Roaming\MetaQuotes\Terminal\76F5A6AB6C9C072B94A014923F71D84B\MQL4\Files\Terminal_700000034.json"
 }
 
 @app.get("/")

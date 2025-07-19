278ea1dd1a80efdaf18715a5d9c749dd8e5e93b6
Bhairav Sharad <bhairav.sharad@gmail.com>
2025-07-19 15:38:00 -0700

    Added the remainder of the 3 terminals to the display

--- app/main.py
+++ app/main.py
@@ -7,11 +7,10 @@ app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
 TERMINALS = {
-    "Terminal 1": "C:/Users/3112923-2/AppData/Roaming/MetaQuotes/Terminal/5D49F47D1EA1ECFC0DDC965B6D100AC5/MQL4/Files/Terminal_60044391.json",
-    # Uncomment other terminals as needed:
-    # "Terminal 2": "C:/Users/3112923-2/AppData/Roaming/MetaQuotes/Terminal/6D8ED79B81D9D70041DDDF6786110ECE/MQL4/Files/Terminal_50055879.json",
-    # "Terminal 3": "C:/Users/3112923-2/AppData/Roaming/MetaQuotes/Terminal/704444BB97FFEAFF0FAD48FB79A73215/MQL4/Files/Terminal_700000032.json",
-    # "Terminal 4": "C:/Users/3112923-2/AppData/Roaming/MetaQuotes/Terminal/76F5A6AB6C9C072B94A014923F71D84B/MQL4/Files/Terminal_700000034.json",
+    "Basic": "C:/Users/3112923-2/AppData/Roaming/MetaQuotes/Terminal/5D49F47D1EA1ECFC0DDC965B6D100AC5/MQL4/Files/Terminal_60044391.json",
+    "Surge": "C:/Users/3112923-2/AppData/Roaming/MetaQuotes/Terminal/76F5A6AB6C9C072B94A014923F71D84B/MQL4/Files/Terminal_700000034.json",
+    "HNW":   "C:/Users/3112923-2/AppData/Roaming/MetaQuotes/Terminal/6D8ED79B81D9D70041DDDF6786110ECE/MQL4/Files/Terminal_50055879.json",
+    "VT30":  "C:/Users/3112923-2/AppData/Roaming/MetaQuotes/Terminal/704444BB97FFEAFF0FAD48FB79A73215/MQL4/Files/Terminal_700000032.json",
 }
 
 @app.get("/")
